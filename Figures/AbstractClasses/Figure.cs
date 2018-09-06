4fd9a67a322514dec2e81aef3483d8dc9444ef21
Ilia <blackil@yandex.ru>
2018-09-06 22:50:27 +0300

    Add files via upload

---
+++ Figures/AbstractClasses/Figure.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindBoxTest_Libary.Figures.AbstractClassesAndInterfaces
+{
+    // Так как в задаче сказано что есть юнит- тест на добавление новых фигур,
+    // я делаю вывод что  клиент для вычислений будет использовать сущности, а не набор параметров(длина, ширина, и тд)
+    /// <summary>
+    /// Класс, выделяющий абстракцию для геометрической фигуры
+    /// </summary>
+    public abstract class Figure
+    {
+        /// <summary>
+        /// Площадь фигуры
+        /// </summary>
+        public abstract double Square { get;}
+        // Задаю что длины(радиус) для фигур могут быть приведены к double
+        // Надеюсь это не очень страшно
+        // Так же не делаю обобщенную проверку на то что параметры  меньше или равны нулю,
+        // т к возможно захотим создавать другие фигуры где корректность
+        // проверяется соотношением величин ее сторон 
+        /// <summary>
+        ///  Проверка корректны ли входные параметры для фигуры
+        /// </summary>
+        /// <param name="args">Входные параметры</param>
+        /// <returns></returns>
+        protected abstract bool IfInputParamsOk(params double[] args);
+    }
+}
---
+++ Figures/Figures/Triangle.cs
@@ -0,0 +1,78 @@
+﻿using MindBoxTest_Libary.Exceptions;
+using MindBoxTest_Libary.Figures.AbstractClassesAndInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindBoxTest_Libary.Figures.Figures
+{
+    public class Triangle : Figure
+    {
+        private readonly bool _isRightAngled;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="aSide">Сторона 1</param>
+        /// <param name="bSide">Сторона 2</param>
+        /// <param name="cSide">Сторона 3</param>
+        public Triangle(double aSide, double bSide, double cSide)
+        {
+            if (IfInputParamsOk(aSide, bSide, cSide) == false)
+                throw new InvalidInputForFigureException(this.GetType());
+
+            ASide = aSide;
+            BSide = bSide;
+            CSide = cSide;
+
+            //Высчитываем является треугольник прямоугольным
+
+            //Чтобы не делать много if
+            double[] sides = new double[3] { ASide, BSide, CSide };
+
+            double biggestSide = sides.Max();
+            double[] otherSides = sides.Where(side => side != biggestSide).ToArray();
+            double sumOfSqrtsOfOtherSides = otherSides.Sum(side => Math.Pow(side, 2));
+
+            _isRightAngled = (Math.Pow(biggestSide, 2) == sumOfSqrtsOfOtherSides);
+        }
+
+        protected override bool IfInputParamsOk(params double[] args)
+        {
+            foreach (double arg in args)
+            {
+                if (arg <= 0)
+                    return false;
+            }
+            return true;
+        }
+
+        public override double Square
+        {
+            get
+            {
+                double halfOfPerimeter = (ASide + BSide + CSide) / 2;
+                double m1 = halfOfPerimeter - BSide;
+                double m2 = halfOfPerimeter - CSide;
+                double m3 = halfOfPerimeter - ASide;
+                return Math.Sqrt(halfOfPerimeter * m1 * m2 * m3);
+            }
+        }
+        /// <summary>
+        /// Является ли треугольник прямоугольным
+        /// </summary>
+        public bool IsRightAngled
+        {
+            get
+            {
+                return _isRightAngled;
+            }
+        }
+        public double ASide { get; private set; }
+        public double BSide { get; private set; }
+        public double CSide { get; private set; }
+     
+
+    }
+}
